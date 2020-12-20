c9deed37f4b3951e2b2c63bf17faf14f387747a1
Julian Dunskus <julian.dunskus@gmail.com>
2020-12-20 21:59:22 +0100

    update dev env to 1.16, use @Inject rather than @Redirect
    hopefully this makes it compatible with canvas

--- src/main/java/retino/mixin/WindowHintsMixin.java
+++ src/main/java/retino/mixin/WindowHintsMixin.java
@@ -4,19 +4,19 @@ import net.minecraft.client.util.Window;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = Window.class)
 public abstract class WindowHintsMixin {
-	@Redirect(
+	@Inject(
 		at = @At(
 			value = "INVOKE",
-			target = "Lorg/lwjgl/glfw/GLFW;glfwDefaultWindowHints()V"
+			target = "Lorg/lwjgl/glfw/GLFW;glfwCreateWindow(IILjava/lang/CharSequence;JJ)J"
 		),
 		method = "<init>"
 	)
-	private void redirectDefaultWindowHints() {
-		GLFW.glfwDefaultWindowHints();
+	private void adjustWindowHints(CallbackInfo callbackInfo) {
 		GLFW.glfwWindowHint(GLFW.GLFW_COCOA_RETINA_FRAMEBUFFER, GLFW.GLFW_FALSE);
 	}
 }
